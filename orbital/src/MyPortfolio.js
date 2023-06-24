6f1e10dbf855f5308a6d57e9d498aa7fbc900edf
andrew250402 <104449525+andrew250402@users.noreply.github.com>
2023-06-25 02:17:16 +0800

    Add files via upload

---
+++ orbital/src/MyPortfolio.js
@@ -0,0 +1,31 @@
+import Side from "./Home/Side";
+import "./MyPortfolio.css";
+import PortfolioList from "./PortfolioList";
+import { useReview } from "./hooks/useReview"
+
+// Brief Overview of Porfolio Details page. Clicking My Portfolio will send them to their specific own portfolio with their own database.
+
+//For the other portion to work, each user in db.json must be linked to their own database (portfolio.json) with their personal mod reviews and programmes. Please do the necessary linkage so from the id in db.json , we can go to the correct portfolio.json data  base and pull out their personal reviews.
+const MyPortfolio = () => {
+  const {
+    reviews: portfolio,
+    isPending,
+    error,
+  } = useReview();
+  console.log(portfolio);
+  return (
+    <div className="container">
+      <div className="side-container">
+        <Side></Side>
+      </div>
+
+      <div className="portfolio-main-container">
+        {error && <div>{error}</div>}
+        {isPending && <div className="port-loading"></div>}
+        {portfolio && <PortfolioList portfolio={portfolio} />}
+      </div>
+    </div>
+  );
+};
+
+export default MyPortfolio;

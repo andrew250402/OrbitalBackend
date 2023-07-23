a312b3b4628b0f048e0cb82dcbb10fed739eab2a
andrew250402 <104449525+andrew250402@users.noreply.github.com>
2023-07-24 02:00:11 +0800

    Delete Orbital Andrew update directory

--- Orbital Andrew update/backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const authController = require('../controllers/authController');
-
-const router = Router();
-
-router.get('/login', authController.login_get);
-router.post('/login', authController.login_post);
-
-module.exports = router;
